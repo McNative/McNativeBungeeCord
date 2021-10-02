345ae9c6d115a10a1216e9c0c5e9e322dc05fc07
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-10-02 22:04:07 +0200

    Added debugs

--- mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
+++ mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
@@ -4,14 +4,22 @@ import net.pretronic.libraries.event.EventPriority;
 import net.pretronic.libraries.event.Listener;
 import net.pretronic.libraries.utility.Iterators;
 import org.mcnative.runtime.api.McNative;
+import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteEvent;
 import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteResponseEvent;
+import org.mcnative.runtime.api.protocol.packet.type.player.complete.MinecraftPlayerTabCompleteResponsePacket;
 
 import java.util.List;
 
 public class TabCompleteInjectListener {
 
     @Listener(priority = EventPriority.LOW)
+    public void handleTabComplete(MinecraftPlayerTabCompleteEvent event){
+        System.out.println("Request: "+event.getCursor());
+    }
+
+    @Listener(priority = EventPriority.LOW)
     public void handleTabComplete(MinecraftPlayerTabCompleteResponseEvent event){
+        System.out.println("Response: "+event.getCursor());
         if(event.getCursor() == null)return;
         String cursor = (event.getCursor().length() > 0 && event.getCursor().charAt(0) == '/' ? event.getCursor().substring(1) : event.getCursor()).toLowerCase();
         List<String> completion = Iterators.map(McNative.getInstance().getLocal().getCommandManager().getCommands()
@@ -21,6 +29,7 @@ public class TabCompleteInjectListener {
             return name.startsWith(cursor) && !event.getSuggestions().contains(name);
         });
         event.getSuggestions().addAll(completion);
+        System.out.println("Response: "+event.getSuggestions());
     }
 
 }
