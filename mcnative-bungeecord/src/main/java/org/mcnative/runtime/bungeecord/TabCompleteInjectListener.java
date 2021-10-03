43b321626647daf7d6214237b08e6a29bb3ffbe1
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-10-03 12:58:03 +0200

    Debugs

--- mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
+++ mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
@@ -22,7 +22,12 @@ public class TabCompleteInjectListener {
                         }
                         return false;
                     });
+
+            System.out.println("-------------");
+            System.out.println(event.getSuggestions());
             event.getSuggestions().addAll(completion);
+            System.out.println(event.getSuggestions());
+            System.out.println("-------------");
         }
     }
 
