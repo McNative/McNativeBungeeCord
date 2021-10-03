b5abef2bc4e976f623b49411b0b5c8bf8f77588c
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-10-03 12:43:09 +0200

    Updated tab completion

--- mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
+++ mcnative-bungeecord/src/main/java/org/mcnative/runtime/bungeecord/TabCompleteInjectListener.java
@@ -4,32 +4,26 @@ import net.pretronic.libraries.event.EventPriority;
 import net.pretronic.libraries.event.Listener;
 import net.pretronic.libraries.utility.Iterators;
 import org.mcnative.runtime.api.McNative;
-import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteEvent;
 import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteResponseEvent;
-import org.mcnative.runtime.api.protocol.packet.type.player.complete.MinecraftPlayerTabCompleteResponsePacket;
-
 import java.util.List;
 
 public class TabCompleteInjectListener {
 
     @Listener(priority = EventPriority.LOW)
-    public void handleTabComplete(MinecraftPlayerTabCompleteEvent event){
-        System.out.println("Request: "+event.getCursor());
-    }
-
-    @Listener(priority = EventPriority.LOW)
     public void handleTabComplete(MinecraftPlayerTabCompleteResponseEvent event){
-        System.out.println("Response: "+event.getCursor());
-        if(event.getCursor() == null) return;
-        String cursor = (event.getCursor().length() > 0 && event.getCursor().charAt(0) == '/' ? event.getCursor().substring(1) : event.getCursor()).toLowerCase();
-        List<String> completion = Iterators.map(McNative.getInstance().getLocal().getCommandManager().getCommands()
-                , command -> command.getConfiguration().getName()
-                , command -> {
-            String name = command.getConfiguration().getName().toLowerCase();
-            return name.startsWith(cursor) && !event.getSuggestions().contains(name);
-        });
-        event.getSuggestions().addAll(completion);
-        System.out.println("Response: "+event.getSuggestions());
+        if(event.getCursor() != null && event.getCursor().charAt(0) == '/' && event.getCursor().indexOf(' ') < 0){
+            String cursor = event.getCursor().substring(1).toLowerCase().trim();
+            List<String> completion = Iterators.map(McNative.getInstance().getLocal().getCommandManager().getCommands()
+                    , command -> "/"+command.getConfiguration().getName().toLowerCase()
+                    , command -> {
+                        if(command.getConfiguration().getPermission() == null || event.getPlayer().hasPermission(command.getConfiguration().getPermission())){
+                            String name = command.getConfiguration().getName().toLowerCase();
+                            return name.startsWith(cursor) && !event.getSuggestions().contains("/"+name);
+                        }
+                        return false;
+                    });
+            event.getSuggestions().addAll(completion);
+        }
     }
 
 }
